0dcf966d2b74b6b1ada21bf964af8fce288a9059
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-19 08:18:50 +0100

    Create ui component for Navbar Links

---
+++ frontend/src/components/ui/NavbarLinks.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+
+type NavbarLinksProps = {
+  link: string;
+  title: string;
+  onCloseMenu: () => void;
+};
+
+export default function NavbarLinks({
+  link,
+  title,
+  onCloseMenu,
+}: NavbarLinksProps) {
+  return (
+    <li className="link">
+      <Link to={link} onClick={onCloseMenu}>
+        {title} <IoIosArrowForward className="arrow-link" />
+      </Link>
+    </li>
+  );
+}
