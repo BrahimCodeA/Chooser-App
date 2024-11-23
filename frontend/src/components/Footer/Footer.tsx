e320ed7b0059fde9358f9acd574ce4b877e48f60
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-24 00:05:40 +0100

    create reusable component for FooterSection

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,88 +1,28 @@
-import "./Footer.scss";
 import { useState } from "react";
 import FooterSection from "../ui/FooterSection";
-import { MdMarkEmailUnread } from "react-icons/md";
-import { BsFillTelephoneFill } from "react-icons/bs";
-import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
+import "./Footer.scss";
+import { footerDatas } from "../../constants/footerData";
 
 export default function Footer() {
   const [activeSection, setActiveSection] = useState<string | null>(null);
-  const date = new Date().getFullYear();
 
   const handleToggleSection = (section: string) => {
-    setActiveSection(activeSection === section ? null : section);
+    setActiveSection((prev) => (prev === section ? null : section));
   };
 
   return (
     <footer className="footer">
       <div className="footer-container">
-        <FooterSection
-          title="Contact"
-          isOpen={activeSection === "contact"}
-          onToggle={() => handleToggleSection("contact")}
-        >
-          <p className="footer-items">
-            <MdMarkEmailUnread />
-            support@chooserIb.com
-          </p>
-          <p className="footer-items">
-            <BsFillTelephoneFill />
-            +33 1 23 45 67 89
-          </p>
-        </FooterSection>
-
-        <FooterSection
-          title="Liens utiles"
-          isOpen={activeSection === "liens"}
-          onToggle={() => handleToggleSection("liens")}
-        >
-          <ul>
-            <li>
-              <a className="footer-links-utils" href="/about">
-                À propos
-              </a>
-            </li>
-            <li>
-              <a className="footer-links-utils" href="/faq">
-                FAQ
-              </a>
-            </li>
-            <li>
-              <a className="footer-links-utils" href="/contact">
-                Contactez-nous
-              </a>
-            </li>
-          </ul>
-        </FooterSection>
-
-        <FooterSection
-          title="Suivez-nous"
-          isOpen={activeSection === "suivez-nous"}
-          onToggle={() => handleToggleSection("suivez-nous")}
-        >
-          <ul className="social-media">
-            <li>
-              <a className="social-icon" href="https://facebook.com">
-                <FaFacebookF />
-              </a>
-            </li>
-            <li>
-              <a className="social-icon" href="https://instagram.com">
-                <RiInstagramFill />
-              </a>
-            </li>
-            <li>
-              <a className="social-icon" href="https://linkedin.com">
-                <FaLinkedinIn />
-              </a>
-            </li>
-          </ul>
-        </FooterSection>
-
-        <div className="footer-bottom">
-          <p>&copy; {date} Choose. Tous droits réservés.</p>
-        </div>
+        {footerDatas.map(({ title, content }, index) => (
+          <FooterSection
+            key={index}
+            title={title}
+            isOpen={activeSection === title}
+            onToggle={() => handleToggleSection(title)}
+          >
+            {content}
+          </FooterSection>
+        ))}
       </div>
     </footer>
   );
