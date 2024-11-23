9ce0a869a6faf8806cbfaf5c4901e59b0adbc618
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-24 00:09:51 +0100

    create component for search

--- frontend/src/components/Search/SearchComponent.tsx
+++ frontend/src/components/Search/SearchComponent.tsx
@@ -11,7 +11,7 @@ export default function SearchComponent({
     <div className="search-container show">
       <input type="text" placeholder="Rechercher" />
       <button onClick={onCloseSearch}>Fermer</button>
-      <h2>Les plus populaires</h2>
+      <h2>Les plus populaires actuellement</h2>
     </div>
   );
 }
