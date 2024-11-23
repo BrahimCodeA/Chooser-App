c25b0ad23620f37e8dd321ee4ccb1e75259e5318
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-23 23:32:00 +0100

    imported the slider from config/sliderSettings

--- frontend/src/components/CarouselNav2/CarouselNav2.tsx
+++ frontend/src/components/CarouselNav2/CarouselNav2.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-icons/bi";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { sliderSettings } from "../../config/sliderSettings";
 
 const icons = [
   <BiSearchAlt />,
@@ -33,53 +34,9 @@ const titles = [
 ];
 
 export default function NavbarCarousel() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 5000,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 0,
-    arrows: false,
-    cssEase: "linear",
-    pauseOnHover: true,
-    swipeToSlide: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <nav className="navbar-2">
-      <Slider {...settings}>
+      <Slider {...sliderSettings}>
         {icons.map((icon, index) => (
           <div key={index} className="navbar2-items">
             <span>{icon}</span>
