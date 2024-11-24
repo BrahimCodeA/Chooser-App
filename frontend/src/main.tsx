db67f9d3b72c7fdec13013b6313fd5bc5eae07d7
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-24 01:32:47 +0100

    deplace index.scss link

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
+import "./index.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
 
