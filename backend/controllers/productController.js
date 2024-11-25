6b6190e6f608459126323f1a86fd4d00e38a6892
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-25 23:31:23 +0100

    chnages type of stock to Boolean

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,88 +1,96 @@
-import { v2 as cloudinary } from 'cloudinary';
-import productModel from '../models/productModel.js';
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
 
 const addProduct = async (req, res) => {
-    try {
-        const {
-            name,
-            description,
-            price,
-            category,
-            sizes,
-            bestseller,
-            isDiscounted,
-            discountAmount,
-            stock,
-            brand
-        } = req.body;
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      sizes,
+      bestseller,
+      isDiscounted,
+      discountAmount,
+      stock,
+      brand,
+    } = req.body;
 
-        const image1 = req.files.image1 && req.files.image1[0];
-        const image2 = req.files.image2 && req.files.image2[0];
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
 
-        const images = [image1, image2].filter((item) => item !== undefined);
+    const images = [image1, image2].filter((item) => item !== undefined);
 
-        let imagesUrl = await Promise.all(
-            images.map(async (item) => {
-                let result = await cloudinary.uploader.upload(item.path, {resource_type: 'image'});
-                return result.secure_url;
-            })
-        );
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
 
-        const productData = {
-            name,
-            description,
-            category,
-            price: Number(price),
-            bestseller: bestseller === 'true',
-            sizes: JSON.parse(sizes),
-            isDiscounted: isDiscounted === 'true',
-            discountAmount: Number(discountAmount),
-            stock: Number(stock),
-            brand,
-            image: imagesUrl
-        };
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price),
+      bestseller: bestseller === "true",
+      sizes: JSON.parse(sizes),
+      isDiscounted: isDiscounted === "true",
+      discountAmount: Number(discountAmount),
+      stock: stock === "true",
+      brand,
+      image: imagesUrl,
+    };
 
-        console.log(productData);
+    console.log(productData);
 
-        const product = new productModel(productData);
-        await product.save();
+    const product = new productModel(productData);
+    await product.save();
 
-        res.status(200).json({ success: true, message: "Product added" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Server Error" });
-    }
-}
+    res.status(200).json({ success: true, message: "Product added" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 const listProducts = async (req, res) => {
-    try {
-        const products = await productModel.find({});
-        res.status(200).json({ success: true, products });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const { bestseller, category } = req.query;
+
+    const filter = {};
+    if (bestseller === "true") filter.bestseller = true;
+    if (category) filter.category = category;
+
+    const products = await productModel.find(filter);
+    res.status(200).jsson({ success: true, products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const removeProduct = async (req, res) => {
-    try {
-        await productModel.findByIdAndDelete(req.body.id);
-        res.status(200).json({ success: true, message: "Product removed" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ success: false, message: error.message });
-    }
-}
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.status(200).json({ success: true, message: "Product removed" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const singleProduct = async (req, res) => {
-    try {
-        const { productId } = req.body;
-        const product = await productModel.findById(productId);
-        res.status(200).json({ success: true, product });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: error.message });
-    }
-}
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.status(200).json({ success: true, product });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 export { addProduct, listProducts, removeProduct, singleProduct };
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },  
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true, min: 0 },  
-    image: { type: Array, required: true },    
-    category: { type: String, required: true },                   
-    stock: { type: Number, min: 0 },
+    price: { type: Number, required: true, min: 0 },
+    image: { type: Array, required: true },
+    category: { type: String, required: true },
+    stock: { type: Boolean, default: true },
     brand: { type: String, required: true },
-    sizes: { type: Array, required: true },                
+    sizes: { type: Array, required: true },
     bestseller: { type: Boolean, default: false },
     isDiscounted: { type: Boolean, default: false },
-    discountAmount: { type: Number, min: 0 },      
-}, { timestamps: true });
+    discountAmount: { type: Number, min: 0 },
+  },
+  { timestamps: true }
+);
 
-
-
-const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
 export default productModel;
