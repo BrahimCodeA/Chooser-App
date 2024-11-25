6f030401fe93832d1031926754414c5b622e3ced
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-25 22:04:36 +0100

    change carousal to carousel

--- frontend/src/components/CarouselNav2/CarouselNav2.tsx
+++ frontend/src/components/CarouselNav2/CarouselNav2.tsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { sliderSettings } from "../../config/sliderSettings";
-import { icons, titles } from "../../constants/carousalNav2Data";
+import { icons, titles } from "../../constants/carouselNav2Data";
 
 export default function NavbarCarousel() {
   return (
