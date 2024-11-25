9c346b164c3219519543ed6aaa37394e5f9d8192
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-11-25 22:04:59 +0100

    add BestSellerCarousel Component

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,12 +1,12 @@
+import BestSellerCarousel from "../BestSellerCarousel/BestSellerCarousel";
 import HeroComponent from "../Hero/Hero";
-import ProductCarousel from "../ProductCarousel/ProductCarousel";
 import "./Main.scss";
 
 export default function Main() {
   return (
     <div className="main-container">
       <HeroComponent />
-      <ProductCarousel />
+      <BestSellerCarousel />
     </div>
   );
 }
