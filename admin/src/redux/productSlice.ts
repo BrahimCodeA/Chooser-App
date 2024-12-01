3a19bc333bb1abc1fab705902ce2e00f4e4bb6ac
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-01 22:22:18 +0100

    add Redux slice to product

--- admin/src/redux/productSlice.ts
+++ admin/src/redux/productSlice.ts
@@ -1,42 +1,46 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 type Product = {
-    _id: string;
-    name: string;
-    description: string;
-    price: number;
-    stock: number;
-    category: string;
-    brand: string;
-    sizes: string[];
-    bestseller: boolean;
-    isDiscounted: boolean;
-    discountAmount: number;
-    image: string[];
-}
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: boolean;
+  category: string;
+  brand: string;
+  sizes: string[];
+  bestseller: boolean;
+  isDiscounted: boolean;
+  discountAmount: number;
+  image: string[];
+};
 
 type ProductState = {
-    products: Product[];
-}
+  products: Product[];
+};
 
 const initialState: ProductState = {
-    products: [],
-}
+  products: [],
+};
 
 const productSlice = createSlice({
-    name: "product",
-    initialState,
-    reducers: {
-        addProduct(state, action: PayloadAction<Product>) {
-            state.products.push(action.payload);
-        },
-        deleteProduct(state, action: PayloadAction<string>) {
-            state.products = state.products.filter(product => product._id !== action.payload);
-        },
-        setProducts(state, action: PayloadAction<Product[]>) {
-            state.products = action.payload;
-        },
+  name: "product",
+  initialState,
+  reducers: {
+    addProduct(state, action: PayloadAction<Product>) {
+      state.products.push(action.payload);
     },
+
+    deleteProduct(state, action: PayloadAction<string>) {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload
+      );
+    },
+
+    setProducts(state, action: PayloadAction<Product[]>) {
+      state.products = action.payload;
+    },
+  },
 });
 
 export const { addProduct, deleteProduct, setProducts } = productSlice.actions;
