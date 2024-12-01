6dd228dbbe7807365cd9dd69654589a3c422a1c2
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-01 22:23:11 +0100

    create adminAuth with jwt token

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,20 +1,24 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const adminAuth = async (req, res, next) => {
-    try {
-        const { token } = req.headers;
-        if (!token) {
-            return res.status(401).json({ success: false, message: "Not Authorized" });
-        }
-        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            return res.status(401).json({ success: false, message: "Not Authorized" });
-        }
-        next();
-    } catch (error) {
-        console.error(error);
-        res.status(401).json({ message: "Not Authorized" });
+  try {
+    const { token } = req.headers;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not Authorized" });
     }
-}
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not Authorized" });
+    }
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Not Authorized" });
+  }
+};
 
 export default adminAuth;
