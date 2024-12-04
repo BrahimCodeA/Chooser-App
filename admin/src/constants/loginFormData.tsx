0c05adbfc80b5819a5afe0a05d046ca1898a7d3d
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-04 08:42:39 +0100

    Add: define labels and input fields for form

---
+++ admin/src/constants/loginFormData.tsx
@@ -0,0 +1,5 @@
+export const labels = ["Adress email", "Mot de passe"];
+export const inputs = [
+  { type: "email", placeholder: "choose@gmail.com", name: "email" },
+  { type: "password", placeholder: "***************", name: "password" },
+];
